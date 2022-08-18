5f76198db7de4c3c5d8a8b70ced8bd3c528e878f
victorbrugnolo <brugnolovictor@gmail.com>
2022-08-18 16:39:26 -0300

    create delete function

--- unordered-vector.py
+++ unordered-vector.py
@@ -1,19 +1,20 @@
 import numpy as np
 
+
 class UnorderedVector:
-    def __init__(self, size): 
+    def __init__(self, size):
         self.size = size
         self.last_position = -1
         self.values = np.empty(self.size, dtype=int)
 
-    #O(n)
+    # O(n)
     def print(self):
         if self.last_position == -1:
             print('empty vector')
         else:
             for i in range(self.last_position + 1):
                 print(i, ' - ', self.values[i])
-    
+
     #O(1) - O(2)
     def insert(self, value):
         if self.last_position == self.size - 1:
@@ -22,13 +23,25 @@ class UnorderedVector:
             self.last_position += 1
             self.values[self.last_position] = value
 
-    #O(n)
-    def search(self, value): 
+    # O(n)
+    def search(self, value):
         for i in range(self.last_position + 1):
             if value == self.values[i]:
-                return i 
-            
+                return i
+
         return -1
+
+    # O(n)
+    def delete(self, value):
+        position = self.search(value)
+
+        if position == -1:
+            return -1
+
+        for i in range(position, self.last_position):
+            self.values[i] = self.values[i + 1]
+
+        self.last_position -= 1
 
 
 vector = UnorderedVector(5)
@@ -44,3 +57,16 @@ vector.insert(9)
 vector.print()
 
 print(vector.search(2))
+
+
+vector.delete(4)
+
+vector.print()
+
+vector.delete(2)
+
+vector.print()
+
+vector.delete(9)
+
+vector.print()
